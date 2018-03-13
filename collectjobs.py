5effab9ff502746fd65c8a5ba58ee37635cbfc3c
linan-zhou <zhoulinan14@gmail.com>
2018-03-13 17:17:27 +0800

    initial commit with mysql to save data

--- collectjobs.py
+++ collectjobs.py
@@ -5,6 +5,12 @@ import urllib
 import random
 import requests
 from bs4 import BeautifulSoup
+import pymysql
+
+
+conn = pymysql.connect(host='localhost', user='root', passwd='', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE jobhunting")
 
 ex = urllib.parse.quote('不要求')
 xl = urllib.parse.quote('本科')
@@ -16,22 +22,35 @@ wb_links_elements = driver.find_elements_by_class_name('position_link')
 wb_links = []
 for lk in wb_links_elements:
     wb_links.append(lk.get_attribute('href'))
-ids = []
-company = []
-jobtitle = []
-jobdescription = []
-workaddress = []
+# ids = []
+# company = []
+# jobtitle = []
+# jobdescription = []
+# workaddress = []
 jddr = webdriver.Chrome()
 print(wb_links)
 for urlstring in wb_links:
-    ids.append(re.search(r'https://www.lagou.com/jobs/([0-9]*).html', urlstring).group(1))
+    id = re.search(r'https://www.lagou.com/jobs/([0-9]*).html', urlstring).group(1)
+    # ids.append(id)
+    if cur.execute('''SELECT id from lagoujobs WHERE id = %s''', id):
+        continue
     jddr.get(urlstring)
-    company.append(jddr.find_element_by_class_name('company').text)
-    jobtitle.append(jddr.find_element_by_class_name('job-name').get_attribute('title'))
-    jobdescription.append(jddr.find_element_by_class_name('job_bt').text)
-    workaddress.append(jddr.find_element_by_class_name('work_addr').text)
-    a = random.uniform(10, 15)
+    print('id:', id)
+    company = jddr.find_element_by_class_name('company').text
+    jobtitle = jddr.find_element_by_class_name('job-name').get_attribute('title')
+    jobdescription = jddr.find_element_by_class_name('job_bt').text
+    workaddress = jddr.find_element_by_class_name(' work_addr').text
+    cur.execute('''
+                INSERT INTO lagoujobs (id, company, jobtitle, jd, address)
+                VALUES (%s, %s, %s, %s, %s)''',
+                (id, company, jobtitle, jobdescription, workaddress))
+    a = random.uniform(5, 10)
+    print('sleep:', a)
     time.sleep(a)
+
+cur.connection.commit()
+cur.close()
+conn.close()
 
 
 
